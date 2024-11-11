8cfa8f64fc22f8d4b528020cc5d3840745f886de
Harshil Vaghasiya <harshil.kmphitech@gmail.com>
2024-11-11 15:13:03 +0530

    add Image optimizer

---
+++ lib/app/ui/widgets/custom_image_view.dart
@@ -0,0 +1,153 @@
+// ignore_for_file: must_be_immutable, deprecated_member_use
+
+import 'package:flutter/material.dart';
+
+class CustomImageView extends StatelessWidget {
+  ///[imagePath] is required parameter for showing image
+
+  String? imagePath;
+  Uint8List? imageData;
+  double? height;
+  double? width;
+  Color? color;
+  BoxFit? fit;
+  Alignment? alignment;
+  VoidCallback? onTap;
+  EdgeInsetsGeometry? margin;
+  BorderRadius? radius;
+  BoxBorder? border;
+
+  CustomImageView({
+    super.key,
+    this.imagePath,
+    this.imageData,
+    this.height,
+    this.width,
+    this.color,
+    this.fit,
+    this.alignment,
+    this.onTap,
+    this.radius,
+    this.margin,
+    this.border,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return alignment != null
+        ? Align(
+            alignment: alignment!,
+            child: _buildWidget(),
+          )
+        : _buildWidget();
+  }
+
+  Widget _buildWidget() {
+    return Padding(
+      padding: margin ?? EdgeInsets.zero,
+      child: InkWell(
+        onTap: onTap,
+        child: _buildCircleImage(),
+      ),
+    );
+  }
+
+  _buildCircleImage() {
+    if (radius != null) {
+      return ClipRRect(
+        borderRadius: radius ?? BorderRadius.zero,
+        child: _buildImageWithBorder(),
+      );
+    } else {
+      return _buildImageWithBorder();
+    }
+  }
+
+  _buildImageWithBorder() {
+    if (border != null) {
+      return Container(
+        decoration: BoxDecoration(
+          border: border,
+          borderRadius: radius,
+        ),
+        child: _buildImageView(),
+      );
+    } else {
+      return _buildImageView();
+    }
+  }
+
+  Widget _buildImageView() {
+    if (imagePath != null) {
+      switch (imagePath!.imageType) {
+        case ImageType.svg:
+          return SizedBox(
+            height: height,
+            width: width,
+            child: SvgPicture.asset(
+              imagePath!,
+              height: height,
+              width: width,
+              fit: fit ?? BoxFit.contain,
+              color: color,
+            ),
+          );
+        case ImageType.file:
+          return Image.file(
+            File(imagePath!),
+            height: height,
+            width: width,
+            fit: fit ?? BoxFit.cover,
+            color: color,
+          );
+        case ImageType.network:
+          return CachedNetworkImage(
+              height: height,
+              width: width,
+              fit: fit,
+              imageUrl: imagePath!,
+              color: color,
+              placeholder: (context, url) => SizedBox(
+                    height: 30,
+                    width: 30,
+                    child: LinearProgressIndicator(
+                      color: Colors.grey.shade200,
+                      backgroundColor: Colors.grey.shade100,
+                    ),
+                  ),
+              errorWidget: (context, url, error) =>
+                  Assets.images.png.imageNotFound.image(
+                    height: height,
+                    width: width,
+                    fit: fit ?? BoxFit.cover,
+                  ));
+        case ImageType.png:
+        default:
+          return Image.asset(
+            imagePath!,
+            height: height,
+            width: width,
+            fit: fit ?? BoxFit.cover,
+            color: color,
+          );
+      }
+    }
+    return const SizedBox.shrink();
+  }
+}
+
+extension ImageTypeExtension on String {
+  ImageType get imageType {
+    if (startsWith('http') || startsWith('https')) {
+      return ImageType.network;
+    } else if (endsWith('.svg')) {
+      return ImageType.svg;
+    } else if (startsWith('file://')) {
+      return ImageType.file;
+    } else {
+      return ImageType.png;
+    }
+  }
+}
+
+enum ImageType { svg, png, network, file, unknown }
