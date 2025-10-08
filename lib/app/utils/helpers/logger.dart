3aef19c5e5f6490f911e68f506f72e9263bf888e
Jay-P-Kmphitech <jayp.kmphitech@gmail.com>
2025-10-08 10:42:52 +0530

    More detailed log output integrated

--- lib/app/utils/helpers/logger.dart
+++ lib/app/utils/helpers/logger.dart
@@ -1,8 +1,22 @@
 import 'dart:developer' as dev;
 
+import 'package:dio/dio.dart';
+
 extension LoggerExtension<T> on T {
   T get log {
-    dev.log(toString());
+    if (this case (final Object e, final StackTrace stackTrace)) {
+      dev.log(e.toString(), stackTrace: stackTrace);
+    } else if (this case final StackOverflowError e) {
+      dev.log(e.toString(), stackTrace: e.stackTrace);
+    } else if (this case final ArgumentError e) {
+      dev.log(e.toString(), stackTrace: e.stackTrace);
+    } else if (this case final DioException e) {
+      dev.log(e.toString(), stackTrace: e.stackTrace);
+    } else if (this case final TypeError e) {
+      dev.log(e.toString(), stackTrace: e.stackTrace);
+    } else {
+      dev.log(toString());
+    }
     return this;
   }
 
